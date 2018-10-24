ae670b3b49179c8e48ac5434b04ce5aad0248451
Steven Duncan <steven.k.duncan@gmail.com>
2018-10-24 10:41:51 -0500

    deleted extra knexfile

--- db/seeds/dev/tables.js
+++ db/seeds/dev/tables.js
@@ -1,22 +1,13 @@
-// created from shell command knex seed:make tables
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('users').del()
+  return knex('table_name').del()
     .then(function () {
       // Inserts seed entries
-      return knex('users').insert([
-        {id: 1, name: 'steven', weight: 185, height: 6, age: 30}
+      return knex('table_name').insert([
+        {id: 1, colName: 'rowValue1'},
+        {id: 2, colName: 'rowValue2'},
+        {id: 3, colName: 'rowValue3'}
       ]);
-    })
-    .then(() => {
-      return knex('sleep').insert([
-        {id: 1, user: 1, hourCount: 8, startHour: '02:05 PM', endHour: '10:05 PM', nightSlept: '2018-08-22'}
-      ])
-    })
-    .then(() => {
-      return knex('calories').insert([
-        {id: 1, user: 1, calories: 200, timeAte: '10:10 AM', dayAte: '2018-10-22', foodAte: 'cheeseburger'}
-      ])
-    })
+    });
 };
