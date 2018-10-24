d41f99b9c2d0622bc4968c95eabe4657ee1033e8
Steven Duncan <steven.k.duncan@gmail.com>
2018-10-24 13:37:44 -0500

    reset table seed file

--- db/seeds/dev/tables.js
+++ db/seeds/dev/tables.js
@@ -1,13 +1,35 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+      return knex('users').insert([{
+        id: 1,
+        name: 'steven',
+        weight: 185,
+        height: 6,
+        age: 30
+      }]);
+    })
+    .then(() => {
+      return knex('sleep').insert([{
+        id: 1,
+        user: 1,
+        hourCount: 8,
+        startHour: '02:05 PM',
+        endHour: '10:05 PM',
+        nightSlept: '2018-08-22'
+      }])
+    })
+    .then(() => {
+      return knex('calories').insert([{
+        id: 1,
+        user: 1,
+        calories: 200,
+        timeAte: '10:10 AM',
+        dayAte: '2018-10-22',
+        foodAte: 'cheeseburger'
+      }])
+    })
 };
