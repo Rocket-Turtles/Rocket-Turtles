eb44c264f2e431a72cdc92a94a0409f138a30b7f
Steven Duncan <steven.k.duncan@gmail.com>
2018-10-24 13:23:50 -0500

    commit before pull

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sleep from './components/Sleep.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      //sleep states:
+      sleepNights: []
+      
     };
+  }
+
+  //sleep methods:
+  getSleepData() {
+    axios.get('/api/sleep')
   }
 
   render() {
--- server/index.js
+++ server/index.js
@@ -17,6 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../client/dist'));
 
+app.get('/api/sleep')
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
