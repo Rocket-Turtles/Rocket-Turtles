1505e95977ccdec0267c06aff40be1a720888377
Rikki Sasda <rikkisasda@gmail.com>
2018-10-25 16:18:01 -0500

    blob init

---
+++ client/src/components/BlobBuddy.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Blob extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Blob: {
+        name: '',
+        tiredness: 0
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Blob;
